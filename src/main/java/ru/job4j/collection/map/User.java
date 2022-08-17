943fc3ace99e6a6a33f4614a778665c70c1c3737
Sergei Begletsov <snower87@yandex.ru>
2022-08-17 14:45:42 +0500

    Добавил несколько методов в п.3. Переопределить только hashCode [#1003] (ver.2)

--- src/main/java/ru/job4j/collection/map/User.java
+++ src/main/java/ru/job4j/collection/map/User.java
@@ -18,14 +18,27 @@ public class User {
         return Objects.hash(name, children, birthday);
     }
 
+    // the "rehash" function in JAVA 8 that directly takes the key
+    final int hash(Object key) {
+        int h;
+        return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
+    }
+
+    // the function that returns the index from the rehashed hash
+    int indexFor(int h, int length) {
+        return h & (length - 1);
+    }
+
     public static void main(String[] args) {
         Calendar dateBirth = new GregorianCalendar(1982, Calendar.AUGUST, 30);
         dateBirth.set(Calendar.MILLISECOND, 29);
         User user1 = new User("Anton", 2, dateBirth);
         User user2 = new User("Anton", 2, dateBirth);
+        User user3 = new User("Anton", 2, dateBirth);
         Map<User, Object> hashMap = new HashMap<>();
         hashMap.put(user1, new Object());
         hashMap.put(user2, new Object());
+        hashMap.put(user3, new Object());
         System.out.println(hashMap);
         for (User user: hashMap.keySet()) {
             System.out.println("user:" + user);
@@ -47,6 +60,15 @@ public class User {
         System.out.println("Индекс бакета user2 = " + i2);
         System.out.println();
 
+        System.out.format("user3 hashCode (в hex) = %05x \n", user3.hashCode());
+        System.out.println("user3 hashCode (в 10) = " + user3.hashCode());
+        int h3 = user3.hash(user3);
+        System.out.println("user3 hash = " + h3);
+        int i3 = user3.indexFor(h3, 16);
+        System.out.println("Индекс бакета user3 = " + i3);
+        System.out.println();
+
+        Object x = hashMap.get(user1);
         System.out.println("Size map: " + hashMap.size());
         System.out.println(hashMap.keySet());
     }
