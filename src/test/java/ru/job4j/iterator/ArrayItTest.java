5b79838402d7bfcb0a040409347aedc27a042bbe
Sergei Begletsov <snower87@yandex.ru>
2022-08-18 16:34:45 +0500

    2. Подключение библиотеки AssertJ [#504881]

--- src/test/java/ru/job4j/iterator/ArrayItTest.java
+++ src/test/java/ru/job4j/iterator/ArrayItTest.java
@@ -7,6 +7,8 @@ import java.util.NoSuchElementException;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class ArrayItTest {
     @Test
     public void whenMultiCallhasNextThenTrue() {
@@ -33,5 +35,25 @@ public class ArrayItTest {
                 new int[] {}
         );
         it.next();
+    }
+
+    @Test
+    public void whenMultiCallHasNextThenTrueVer2() {
+        ArrayIt it = new ArrayIt(
+                new int[] {1, 2, 3}
+        );
+        boolean rsl = it.hasNext();
+        assertThat(rsl).isTrue();
+        assertThat(it.hasNext()).isTrue();
+    }
+
+    @Test
+    public void whenReadSequenceVer2() {
+        ArrayIt it = new ArrayIt(
+                new int[] {1, 2, 3}
+        );
+        assertThat(it.next()).isEqualTo(1);
+        assertThat(it.next()).isEqualTo(2);
+        assertThat(it.next()).isEqualTo(3);
     }
 }
